a8d3e7b7801a48aa7088fa27b7bd31448ce5d13f
Anajujuba007 <110496191+Anajujuba007@users.noreply.github.com>
2022-10-26 17:40:51 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -30,28 +30,26 @@ function quadrado(){
     for(var i = 2; i < 11; i ++){
         document.write("O quadrado de" + i + " é " + (i*i)+"<br>");
     }
+
+}
+function moeda(atual){
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 }
 
 function total(){
     let val = document.getElementById("valor").value;
     let ju = document.getElementById("juros").value;
+    
     let t = document.getElementById("meses").value;
     let resultado = 0;
-    for(let m = 1; m <= t; m++)
-        resultado = (val * ((ju/100)+1))
+    let saida = "";
+    for(let m = 1; m <= t; m++){
+        resultado = (val * ((ju/100)+1));
         val = resultado;
-        document.write("Mes " + m + " valor de " + val)
-}
-    document.write(0 total e de: "")
-    if (Number(val)){
-        alert("O valor deve ser um numer0.");
-        document.getElementById("valor").value = "";
-        document.getElementById("valor").focus();
-        return
+        saida += "Mês " + m + ": " + moeda(val) + "<br>";
+       // document.write("Mês " + m + " valor de " + moeda (val) + "<br>");
     }
-    
-    document.write("O total é de: " + resultado);
+    document.getElementById("mes").innerHTML = saida;
+    document.getElementById("resultado").innerHTML = "Total" + moeda(resultado);
+    //document.write("O total é de: " + moeda (resultado) );
 }
-
-
-
